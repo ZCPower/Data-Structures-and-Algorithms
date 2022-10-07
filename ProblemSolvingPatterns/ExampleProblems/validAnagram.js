68da4e8809ad74af8fd18c54f309ff3cf0152584
ZCPower <zachpowerxc@gmail.com>
2022-10-06 21:55:18 -0500

    completed one practice problem

---
+++ ProblemSolvingPatterns/ExampleProblems/validAnagram.js
@@ -0,0 +1,77 @@
+/* Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging letters of another, such as cinema, formed from iceman. */
+
+//I see two possible solutions. 
+/* Solution One
+    -before we create loop, check that strs are the same length if not return false
+    -create a loop that iterates through loop 1 and checks arr2.indexOf(arr[i])
+    - if this is -1, we return false.
+    - else we would splice that number out of arr2 and continue. if we reach the end of arr1, return true. 
+*/
+
+
+function validAnagram(str1, str2) {
+
+    if (str1.length !== str2.length) {
+        return false
+    }
+
+    for (let i = 0; i < str1.length; i++) {
+        let letter = str1[i]
+        let idx = str2.indexOf(letter)
+
+        if (idx === -1) {
+            return false
+        }
+        else {
+            str2.slice(idx, 1, 'here')
+        }
+    }
+
+    return true
+}
+
+validAnagram('zipza', 'pizza')
+// Time Complexity O(n^2)
+// We create a loop and use .indexOf - which itself loops through the string
+//and creates a nested for loop.
+
+
+/*
+    Second solution: 
+        - Create an obj for each string (ob1, ob2).
+        - create two loops, one for each string.
+        - loop through and check if that letter is in the object. 
+            - if it is, add one to the value, if not set value to one.
+        - create a for in loop to cycle through obj1[letter] and see if it is equal to obj2[letter]
+                - if it is, continue
+                - if not, return false
+*/
+
+function validAnagram2(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false
+    }
+
+    let obj1 = {};
+    let obj2 = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        obj1[str1[i]] ? obj1[str1[i]]++ : obj1[str1[i]] = 1
+    }
+
+    for (let j = 0; j < str2.length; j++) {
+        obj2[str2[j]] ? obj2[str2[j]]++ : obj2[str2[j]] = 1
+    }
+
+    for (let letter in obj1) {
+        if (obj1[letter] !== obj2[letter]) {
+            return false
+        }
+    }
+    return true
+}
+
+validAnagram2('anagramd', 'danagmar')
+
+//Time complexity should reduce to O(n).
+    //We have 3 different loops. so O(3n) reduces to O(n).
---
+++ ProblemSolvingPatterns/Problem Solving Patterns.md
@@ -0,0 +1,42 @@
+# How to improve at algorithms / solving problems? 
+ 
+## 1. Devise a plan for solving problems (Not fool-proof)
+
+### Understand the problem
+    1. Restate the problem in your own words.
+    2. What are inputs that go into the problem?
+    3. What are the outputs we are trying to return?
+    4. Can outputs be determined from the inputs? In other words, do i have enough information to solve the problem?
+    5. How should I label the important pieces of data that are part of the problem?
+
+### Explore Concrete Examples
+    - Start with simple examples
+    - Progress to more complex examples
+    - Explore examples to empty inputs
+    - Explore examples for invalid inputs
+
+### Break it Down
+    -Explicitly write out the steps you need to take.
+        - Forces you to think about your code before you actually write it. 
+        - Catches any lingering uncertainties.
+        - Gives a roadmap for your function/algorithm. 
+
+### Solve/Simplify
+
+### Look Back and Refactor
+
+## 2. Master common problem solving patterns, Common Patterns Include:
+
+
+### Frequency Counters
+        - Use an object to collect a bunch of values(as keys) and their frequencies( as values)
+        - This helps avoid the need for nested loops. Which would be a O(n^2) operation with strings/arrays.
+        - Two seperate loops vastly better than a nested loop. O(2n) vs. O(n^2).
+        
+###  Multiple Pointers
+###  Sliding Window
+### Divide and Conquer
+###  Dynammic Programming
+###  Greedy Algorithms
+###  Backtracking
+###  Many more... 
